3e72caedb9fce867204ed1f900dc664c491907d5
Mason <Mudland2721@gamil.com>
2020-09-07 11:15:08 -0600

    file structure

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function App() {
+    return (
+        <>
+        //nav
+        //main page
+        //footer of something
+        </>
+    );
+}
+
+export default App;
